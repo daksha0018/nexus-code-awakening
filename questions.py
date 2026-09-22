43c15b17a0a01adb05b5327cffd0e5800b1ebbab
Daksha <daksha0018@gmail.com>
2026-09-22 23:08:31 +0530

    Add more coding questions #1

--- questions.py
+++ questions.py
@@ -4,16 +4,19 @@ questions = [
         "options": ["Queue", "Stack", "Array", "Graph"],
         "answer": 2
     },
+
     {
         "question": "Which language is commonly used for machine learning?",
         "options": ["Python", "HTML", "CSS", "XML"],
         "answer": 1
     },
+
     {
         "question": "Which SQL command is used to retrieve data?",
         "options": ["INSERT", "UPDATE", "SELECT", "DELETE"],
         "answer": 3
     },
+
     {
         "question": "What does CPU stand for?",
         "options": [
@@ -24,6 +27,7 @@ questions = [
         ],
         "answer": 1
     },
+
     {
         "question": "Which algorithm is commonly used for searching?",
         "options": [
@@ -32,6 +36,18 @@ questions = [
             "Bubble Sort",
             "DFS"
         ],
+        "answer": 2
+    },
+
+    {
+        "question": "Which keyword is used to define a function in Python?",
+        "options": ["func", "define", "def", "function"],
+        "answer": 3
+    },
+
+    {
+        "question": "Which data structure uses FIFO?",
+        "options": ["Stack", "Queue", "Tree", "Graph"],
         "answer": 2
     }
 ]
